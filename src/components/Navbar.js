2f86dba279d890c90a4532b03e356302e3944ec0
Nicolae Pop <76559047+NicolaePop117@users.noreply.github.com>
2021-05-14 00:44:44 +0200

    going to lesson3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() { 
-    return ( 
-      <nav>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+    const{isLightTheme, light, dark} = this.context;
+    const theme = isLightTheme ? light :dark;
+    return (       
+      <ThemeContext.Consumer>{(context)=>{
+         
+      }}
+        <nav style={{ background:theme.ui, color:theme.syntax }}>
+          <h1>Context App</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li> 
+          </ul>
+        </nav>
+      </ThemeContext.Consumer>
     );
   }
 }
